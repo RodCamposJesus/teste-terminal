a8e89102338662a492a892644fc4ee24b621fa3b
Rodrigo <chocoliris@msn.com>
2025-02-14 08:11:50 -0300

    teste scanner

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,12 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
         //os argumentos começam com o índice 0
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura  = Double.valueOf(args[3]);
 
-        System.out.println("Ola me chamo " + nome + "");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+
+        System.out.println("Digite o seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobre nome ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite a sua idade ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite a sua altura ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Ola me chamo " + nome + "" + sobrenome);
         System.out.println("Tenho " + idade + " anos");
         System.out.println("Minha altura é " + altura + "");
     }
